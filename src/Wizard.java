099d23d7e4d929bea48537a9e37b0b88fb3df368
WilmarCalzado <100540672+WilmarCalzado@users.noreply.github.com>
2022-05-12 15:22:36 +0800

    initial-commit

---
+++ src/Wizard.java
@@ -0,0 +1,10 @@
+public class Wizard {
+    Wizard(String name){
+        super(name);
+    }
+    public void fireStorm(Character enemyCharacter){
+        System.out.println(super.characterName + "attacks " + enemyCharacter.characterName + " with Firestorm (Damage - 50)");
+        int damagePoints = 50;
+        damageTarget(enemyCharacter, damagePoints);
+    };
+}
